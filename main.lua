71e58da8e77d3a553a29f9bf22eb6dc954541562
spectrenoir06 <antoine@doussaud.org>
2015-02-06 23:43:38 +0100

    push

---
+++ main.lua
@@ -0,0 +1,30 @@
+--[[ **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    main.lua                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2014/07/18 23:14:44 by adoussau          #+#    #+#              #
+#    Updated: 2014/07/18 23:20:06 by adoussau         ###   ########.fr        #
+#                                                                              #
+# ***************************************************************************--]]
+
+local Game = require "class/Game"
+
+function love.load()
+
+end
+
+
+function love.draw()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.mousepressed(x,y,button)
+
+end
