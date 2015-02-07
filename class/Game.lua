9b06fe32dc32cf214ffa1fe1b7fb254251f54345
Vallium <aalliot@student.42.fr>
2015-02-07 21:36:28 +0100

    stairs fix

--- class/Game.lua
+++ class/Game.lua
@@ -17,7 +17,6 @@ function Game:initPerso()
 		for x=0, self.map.layers[1].width-1 do
 			print(x,y,self.map.layers[1].data[x][y])
 			if self.map.layers[1].data[x][y] == 4 then
-				self.map.layers[1].data[x][y] = 1
 				self.perso.posX = 32 * x
 				self.perso.posY = 32 * y
 				break
@@ -25,7 +24,6 @@ function Game:initPerso()
 		end
 	end
 	self.perso:popUnfog()
-	self.map:update(1)
 end
 
 function Game:update(dt)
