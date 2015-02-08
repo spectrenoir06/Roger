ebf8aa0f8a6cf16642888532c74c45cee4bb055a
spectrenoir06 <antoine@doussaud.org>
2015-02-08 10:51:14 +0100

    qsdsqd

--- gamestate/ville.lua
+++ gamestate/ville.lua
@@ -2,25 +2,15 @@ ville = {}
 local class = require 'class/middleclass'
 local Gamestate = require 'class/gamestate'
 
-local ffi = require("ffi")
+tab = {}
 
 function ville:enter()
-    tab = {}
-    tab.batiment1 = love.graphics.newImage("texture/buildings/castle1.png")
-    tab.batiment2 = love.graphics.newImage("texture/buildings/tower1.png")
-    tab.batiment3 = love.graphics.newImage("texture/buildings/farm2.png")
+    tab[1] = {text = love.graphics.newImage("texture/buildings/castle1.png"), name = "Castle", level = 1, prix = "100$"}
+    tab[2] = {text = love.graphics.newImage("texture/buildings/tower1.png"), name = "Tower", level = 1, prix = "in progress"}
+    tab[3] = {text = love.graphics.newImage("texture/buildings/farm2.png"), name = "Farm", level = 2, prix = "300$"}
     tab.sablier = love.graphics.newImage("texture/buildings/hourglass.png")
     temp2 = os.time() + 666 * 3600
     love.graphics.setNewFont(18)
-
-
-
-    ffi.cdef[[
-        int MessageBoxA(void *w, const char *txt, const char *cap, int type);
-    ]]
-
-    ffi.C.MessageBoxA(nil, "Hello world!", "Test", 0)
-
 end
 
 function ville:update(dt)
@@ -28,12 +18,17 @@ function ville:update(dt)
 end
 
 function ville:draw()
+
+    --print(tab.name)
+    for k,v in ipairs(tab) do
+        print(k,v)
+        love.graphics.draw(v.text, 0, (k-1)*200)
+        love.graphics.print(v.name.."  Level: "..v.level, 200, (k-1) *200 + 50)
+        love.graphics.print("Upgrade: "..v.prix, 200, (k-1) *200 + 75)
+    end
     love.graphics.setBackgroundColor( 39, 53, 53)
-    love.graphics.draw(tab.batiment1, 0, 0)
-    love.graphics.draw(tab.batiment2, 0, 200)
-    love.graphics.draw(tab.batiment3, 0, 400)
-    love.graphics.draw(tab.sablier, 0, 400)
-    love.graphics.print(os.date("%X",temp2 - os.time()), 0,0)
+    love.graphics.draw(tab.sablier, 0, 200)
+    love.graphics.print(os.date("%X",temp2 - os.time()), 200,300)
 
 end
 
