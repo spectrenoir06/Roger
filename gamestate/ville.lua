705c455bdb03bf6a5d7b8275f72133f1d28c1ca3
spectrenoir06 <antoine@doussaud.org>
2015-02-08 11:38:03 +0100

    11h38

--- gamestate/ville.lua
+++ gamestate/ville.lua
@@ -4,12 +4,13 @@ local Gamestate = require 'class/gamestate'
 
 tab = {}
 
-function ville:enter()
-    tab[1] = {text = love.graphics.newImage("texture/buildings/castle1.png"), name = "Castle", level = 1, prix = "100$"}
-    tab[2] = {text = love.graphics.newImage("texture/buildings/tower1.png"), name = "Tower", level = 1, prix = "in progress"}
-    tab[3] = {text = love.graphics.newImage("texture/buildings/farm2.png"), name = "Farm", level = 2, prix = "300$"}
+function ville:init()
+    tab[1] = {text = love.graphics.newImage("texture/buildings/castle1.png"), name = "Castle", level = 1, prix = "100$", time=0}
+    tab[2] = {text = love.graphics.newImage("texture/buildings/tower1.png"), name = "Tower", level = 1, prix = "in progress", time=os.time() + 5844 - (3660)}
+    tab[3] = {text = love.graphics.newImage("texture/buildings/farm2.png"), name = "Farm", level = 2, prix = "300$", time=0}
+    screw = love.graphics.newImage("texture/buildings/screw.png")
     tab.sablier = love.graphics.newImage("texture/buildings/hourglass.png")
-    temp2 = os.time() + 666 * 3600
+    temp2 = os.time() + 5844 - (3660)
     love.graphics.setNewFont(18)
 end
 
@@ -21,14 +22,27 @@ function ville:draw()
 
     --print(tab.name)
     for k,v in ipairs(tab) do
-        print(k,v)
+        --print(k,v)
         love.graphics.draw(v.text, 0, (k-1)*200)
         love.graphics.print(v.name.."  Level: "..v.level, 200, (k-1) *200 + 50)
         love.graphics.print("Upgrade: "..v.prix, 200, (k-1) *200 + 75)
+        if (v.prix ~= "in progress") then
+            love.graphics.draw(screw, 240, (k-1)*200 + 100)
+        else
+            love.graphics.draw(tab.sablier, 0, (k-1)*200)
+            love.graphics.print(os.date("%X",v.time - os.time()), 200,(k-1)*200 + 100)
+        end
+
     end
     love.graphics.setBackgroundColor( 39, 53, 53)
-    love.graphics.draw(tab.sablier, 0, 200)
-    love.graphics.print(os.date("%X",temp2 - os.time()), 200,300)
+end
+
+function ville:mousepressed(x,y,button)
+    print(x,y,button)
+    if (x>= 240 and x < (240+100) and (y>=100 and y<=200 )) then
+        tab[1].prix="in progress"
+        tab[1].time = os.time() + 12674 - (3660)
+    end
 
 end
 
