3273341354624503f7fc62a6e0ff7aec7c1d2292
spectrenoir06 <antoine@doussaud.org>
2015-02-08 13:04:18 +0100

    qsd

--- main.lua
+++ main.lua
@@ -2,6 +2,13 @@ local Gamestate = require 'class/gamestate'
 require 'gamestate/jeu'
 require 'gamestate/ville'
 
+keynb = 0
+ssword1 = 0
+sword2 = 0
+sword3 = 0
+rubynb = 0
+coinnb = 0
+
 function love.load()
 	Gamestate.registerEvents()
 	Gamestate.switch(ville)
