fccf1c87e2f3741a88029edc798c517c8ef9cf2c
spectrenoir06 <antoine@doussaud.org>
2015-02-08 10:28:28 +0100

    qdqsd

--- gamestate/ville.lua
+++ gamestate/ville.lua
@@ -2,9 +2,24 @@ ville = {}
 local class = require 'class/middleclass'
 local Gamestate = require 'class/gamestate'
 
+local ffi = require("ffi")
+
 function ville:enter()
-    batiment1 = love.graphics.newImage("texture/buildings/castle1.png")
-    batiment2 = love.graphics.newImage("texture/buildings/windmill.png")
+    tab = {}
+    tab.batiment1 = love.graphics.newImage("texture/buildings/castle1.png")
+    tab.batiment2 = love.graphics.newImage("texture/buildings/tower1.png")
+    tab.batiment3 = love.graphics.newImage("texture/buildings/farm2.png")
+    tab.sablier = love.graphics.newImage("texture/buildings/hourglass.png")
+    temp2 = os.time() + 666 * 3600
+    love.graphics.setNewFont(18)
+
+
+
+    ffi.cdef[[
+        int MessageBoxA(void *w, const char *txt, const char *cap, int type);
+    ]]
+
+    ffi.C.MessageBoxA(nil, "Hello world!", "Test", 0)
 
 end
 
@@ -13,9 +28,12 @@ function ville:update(dt)
 end
 
 function ville:draw()
-    --love.graphics.setBackgroundColor( 39, 53, 53)
-    love.graphics.draw(batiment1, 0, 0)
-    love.graphics.draw(batiment2, 201, 0)
+    love.graphics.setBackgroundColor( 39, 53, 53)
+    love.graphics.draw(tab.batiment1, 0, 0)
+    love.graphics.draw(tab.batiment2, 0, 200)
+    love.graphics.draw(tab.batiment3, 0, 400)
+    love.graphics.draw(tab.sablier, 0, 400)
+    love.graphics.print(os.date("%X",temp2 - os.time()), 0,0)
 
 end
 
