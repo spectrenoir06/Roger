29bd67bd5fc966d12e16977d8f3b2eee1428e37b
spectrenoir06 <antoine@doussaud.org>
2015-02-08 09:54:58 +0100

    pop

--- gamestate/ville.lua
+++ gamestate/ville.lua
@@ -3,6 +3,8 @@ local class = require 'class/middleclass'
 local Gamestate = require 'class/gamestate'
 
 function ville:enter()
+    batiment1 = love.graphics.newImage("texture/buildings/castle1.png")
+    batiment2 = love.graphics.newImage("texture/buildings/windmill.png")
 
 end
 
@@ -11,13 +13,16 @@ function ville:update(dt)
 end
 
 function ville:draw()
+    --love.graphics.setBackgroundColor( 39, 53, 53)
+    love.graphics.draw(batiment1, 0, 0)
+    love.graphics.draw(batiment2, 201, 0)
 
 end
 
 function ville:keypressed(key)
     print(key)
     if (key == ' ') then
-        Gamestate.switch(jeu)
+        Gamestate.push(jeu)
     elseif key == 'escape' then
         love.event.quit()
     end
