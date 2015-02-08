309e8b1f71e44f60b21fd2ddf0b6ee325a6ff753
Vallium <aalliot@student.42.fr>
2015-02-08 13:28:16 +0100

    popup quit game

--- gamestate/jeu.lua
+++ gamestate/jeu.lua
@@ -15,18 +15,21 @@ function jeu:draw()
     game:draw()
 end
 
+function jeu:quit()
+    game.music:stop()
+    keynb = self.keynb
+    sword1 = self.sword1
+    sword2 = self.sword2
+    sword3 = self.sword3
+    rubynb = self.rubynb
+    coinnb = self.coinnb
+    game.walkSound:stop()
+    Gamestate.pop()
+end
+
 function jeu:keypressed(key)
     if key == 'escape' then
-        game.music:stop()
-        keynb = self.keynb
-        sword1= self.sword1
-        sword2=self.sword2
-        sword3= self.sword3
-        rubynb= self.rubynb
-        coinnb = self.coinnb
-        game.walkSound:stop()
-        Gamestate.pop()
-
+        self:quit()
     else
         print(key)
         game:keypressed(key)
